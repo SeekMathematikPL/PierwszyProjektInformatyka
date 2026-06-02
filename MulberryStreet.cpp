be5941e26296535aee4d84a34b02410625ac9b6d
SeekMathematikPL <seekmathematik@gmail.com>
2026-06-02 18:35:43 +0200

    Pojawila sie druga dokumentacja oraz program (albo bardziej fragment programu)

---
+++ MulberryStreet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int t [20][20];
+int main (){
+    cout << "Autor: Agnieszka Wlodarczyk 1a nr23" << endl;
+    cout << "Gra: Snake" << endl;
+    cout << "Sposob uzycia: klawisze w,a,s,d aby sie poruszac." << endl;
+ //notatka dla mnie:
+ //co musi tu jeszcze powstac:
+ //1.plansza
+ //2.jablka
+ //3.waz
+ //4.cialo weza
+ //5.petla dzieki ktorej by gra dzialala
+ //6.to ze jak waz wjedzie w sciane lub zzajmie cala plansze to gra sie konczy
+ //P.S moze trzeba bylo wybrac cos latwiejszego...?
+}
